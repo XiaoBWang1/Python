c09d2143939d631e3d81800e76d4ed0dd8b4c6f9
XiaoBWang1 <107143569+XiaoBWang1@users.noreply.github.com>
2025-06-29 13:17:19 -0400

    L5 added.

---
+++ Lesson5.py
@@ -0,0 +1,17 @@
+#Logical operator and,or,not
+
+# Assign both integer to temperature and Boolean to their respective input
+temperature = 77
+sunny = True
+
+#If/elif/else statement with logical operator(and/or/not) to determine output
+if temperature >= 70 and sunny:
+    print("It is hot and sunny outside")
+elif temperature <= 50 or sunny:
+    print("It is cold and sunny today.")
+elif temperature <= 0 and not sunny:
+    print("It is freezing and cloudy.")
+elif temperature >= 100 and sunny:
+    print("It is too hot and sunny outside.")
+else:
+    print("Please retry again with the proper input.")

57bca6c64586bf5ee8d3f3994e81b4ad525d3fd5
xbw24 <xbw24@yahoo.com>
2025-07-01 21:05:23 -0400

    9 added.

--- Lesson9.py
+++ Lesson9.py
@@ -1,11 +1,19 @@
 # while Loop, for loop(for counter in (x,range,step)
-# Alarm Clock App
+# Timer Clock App
+# Time library
 import time
 
-alarm_time = input("Please enter the time(minutes): ")
+# Create a integer variable for user input
+alarm_time = int(input("Please enter the time(seconds): "))
 
-for x in range(0, 10):
+# for loop with the user input that counts backward toward till the output display. Time decrement by one second
+# with input mod by 60 seconds in a minute and then mod again for 60 minutes in an hour then also with the hour
+for x in range(alarm_time, 0, -1):
     time.sleep(1)
+    seconds = x % 60
+    minutes = int(x/60) % 60
+    hours = int(x/3600)
+    print(f"{hours:02}:{minutes:02}:{seconds:02}")
 
-
-    print("It is time!")
+# Print the string to on-screen display
+print("It is time!")

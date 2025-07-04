b2cbd99a5f4ee34a443212e0a6842a59dc7f5607
xbw24 <xbw24@yahoo.com>
2025-07-04 17:56:13 -0400

    12 added.

---
+++ Lesson11.py
@@ -0,0 +1,11 @@
+# Keypad App
+
+numPad = ((1,2,3),
+          (4,5,6),
+          (7,8,9),
+          ("*",0,"#"))
+
+for row in numPad:
+    for num in row:
+        print(num, end=" ")
+    print()

743e3954f6b10e1c5e47de86cb665613bde52595
xbw24 <xbw24@yahoo.com>
2025-07-06 16:43:22 -0400

    12 added.

---
+++ Lesson12.py
@@ -0,0 +1,12 @@
+# Dictionary, print(dir(x)), print(help(x)), get(), update(), pop(), popitem(), clear(), keys(), values(), items()
+
+# Create a dictionary of keys of different programming languages with values with its creators
+programmingLanguage = ("Python : Guido Van Rossum","Java : James Gosling","C++ : Bjarne Stroustrup ",
+                       "C : Dennis Richie","JavaScript : Brendan Eich", "Golang: The Go Authors",
+                       "Rust : Graydon Hoare", "C : Anders Hejlsberg")
+
+# Display in format string of the dictionary
+print(f"{programmingLanguage} are some of the most prominent inventors.")
+
+
+

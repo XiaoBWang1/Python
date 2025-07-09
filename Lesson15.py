7dbe975518690743d7c564d4e643db633ff78fb8
xbw24 <xbw24@yahoo.com>
2025-07-09 13:21:46 -0400

    15 added.

---
+++ Lesson15.py
@@ -0,0 +1,5 @@
+# Positional, default, keyword, arbitrary. * unpacking operator.
+# *args allow multiple non-keyword arguments. **kwargs allows for multiple keyword arguments
+# membership operator (in/not in)
+# list comprehension = [expression / in (value) / in (iterables) / if (condition)]
+# match case = switch case/ case _ = wild card

7eecdfed3cf3b0fd609599f43816a2e569ffba01
xbw24 <xbw24@yahoo.com>
2025-07-20 11:37:40 -0400

    L17 done.

--- Lesson17.py
+++ Lesson17.py
@@ -8,33 +8,33 @@ code = string.ascii_letters + string.digits + string.punctuation
 code = list(code)
 
 # Create a variable for the key by copying from the first variable and shuffling it randomly
-cypher = code.copy()
-random.shuffle(cypher)
+key = code.copy()
+random.shuffle(key)
 
 # Display the randomized code and its encryption
 #print(f"{code}")
-#print(f"{cypher}")
-
+#print(f"{key}")
 
 # Encryption section with a user input, assign empty string for the encrypted message. For every char in list, find the
-# index number of the pre-crypted string and added into the cypher
+# index number of the pre-crypted string and added into the key
 message = input("Enter the texts to encrypt?: ")
 cipher = ""
 
-for text in cypher:
+for text in message:
     index = code.index(text)
-    cipher += cypher[index]
+    cipher += key[index]
 
 print(f"{message}")
 print(f"{cipher}")
 
-# Decryption section which is the reverse of the encryption method
+# Decryption section which is the reverse of the encryption method. Using a for loop to find the indices of the user
+# input in the cypher then append it to the decryption
 encrypted_message = input("Enter the texts to decrypt?: ")
-decode = ""
+decoded_message = ""
 
-for letter in encrypted_message:
-    index = cypher.index(letter)
-    decode += code[index]
+for char in encrypted_message:
+    index = key.index(char)
+    decoded_message += code[index]
 
 print(f"{encrypted_message}")
-print(f"{decode}")
+print(f"{decoded_message}")
--- main.py
+++ main.py
@@ -1,21 +1,7 @@
 # https://www.youtube.com/@BroCodez
 # Python Exercises
 
-import Lesson1
-import Lesson2
-import Lesson3
-import Lesson4
-import Lesson5
-import Lesson6
-import Lesson6
-import Lesson7
-import Lesson8
-import Lesson9
-import Lesson10
-import Lesson11
 import Lesson12
-import Lesson13
-import Lesson14
 import Lesson15
 import Lesson16
 

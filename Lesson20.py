af26ffa579dce80f7f25072c7c9d3cc582454335
xbw24 <xbw24@yahoo.com>
2025-08-21 19:17:25 -0400

    Progress save.

--- Lesson20.py
+++ Lesson20.py
@@ -1,4 +1,5 @@
-""" time library
+"""
+    time library
     import datetime
     datetime.date.today()
     datetime.time(_,_,_)
@@ -13,26 +14,27 @@ import datetime
 import pygame
 
 # Create a function for the alarm action with a formatted print display of the alarm time follow by a Bool for
-# the on state of the alarm clock while adding a mp3 file with its absolute path.
-def time_set(alarm):
-    print(f"Alarm time set at: {alarm}")
+# the on state of the alarm clock while adding a mp3 file with its absolute path
+def time_set(alarm_time):
+    print(f"Alarm time set at: {alarm_time}")
     alarm_sound = "C:\\Users\\xbw24\\Desktop\\Python\\Effects\\Morning_Alarm.mp3"
     running = True
 
 # While loop with Boolean that sets the current time in a formatted string that displays it
+#
     while running:
         real_time = datetime.datetime.now().strftime("%H: %M: %S")
         print(real_time)
-        if real_time == alarm:
+
+        if real_time == alarm_time:
             print("Wake up!")
             running = False
+
         time.sleep(1)
 
 # Dunder main method consists of a user input for setting alarm time along with the time set method function and
 # the alarm argument pass
 if __name__ == "__main__":
-    alarm = input("Set the time(HH:MM:SS): ")
-    time_set(alarm)
-
-
+    alarm_time = input("Set the alarm time(HH:MM:SS): ")
+    time_set(alarm_time)
 

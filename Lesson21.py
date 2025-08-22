3b49caa0b857f41d3fb90877439fbd834980e925
xbw24 <xbw24@yahoo.com>
2025-08-21 20:05:56 -0400

    L21 started.

---
+++ Lesson21.py
@@ -0,0 +1,6 @@
+# Multithreading = threading.Thread(target=my_function), args=(tuple), import threading, thread1.start(), join()
+# multitasking, main thread with other thread concurrently
+
+# Python API request(import requests), install request package(pip install request
+
+
